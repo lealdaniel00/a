1e46e2d93aea4acab91282581ba60cb7111da710
lealdaniel00 <lealdaniel00@gmail.com>
2024-04-30 21:31:33 -0500

    Se ajusta el hola mundo

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,5 +1,9 @@
 public class HolaMundo{
 	public static void main(String [] args){
 		System.out.println("HOLA mundo 123411");
+		int edad = 25;
+		System.out.println("HOLA mundo, mi edad es: "+edad);
+		System.out.println(edad+edad);
+		System.out.println(edad+""+edad);
 	}
 }
