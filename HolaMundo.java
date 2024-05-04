a008537cd9d7a242cdf3d52a2f3285e51cf123da
lealdaniel00 <lealdaniel00@gmail.com>
2024-05-04 09:25:27 -0500

    tercer cambio

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,11 +1,9 @@
 public class HolaMundo{
 	public static void main(String [] args){
-		System.out.println("HOLA mundo 123411");
-		int edad = 25;
-		System.out.println("HOLA mundo, mi edad es: "+edad);
-		System.out.println(edad+edad);
-		System.out.println(edad+""+edad);
 		Persona luis = new Persona();
 		luis.caminar();
+		System.out.println(luis.getEdad());
+		luis.setEdad(35);
+		System.out.println(luis.getEdad());
 	}
 }
--- Persona.java
+++ Persona.java
@@ -1,9 +1,21 @@
 public class Persona{
-	int edad;
-	String nombre;
-	char genero;
-	int estatura;
+	private int edad;
+	private String nombre;
+	private char genero;
+	private int estatura;
+	public Persona(){
+		edad=8;
+		nombre="LUIS";
+		genero='M';
+		estatura=176;
+	}
 	public void caminar(){
 		System.out.println("ESTOY CAMINANDO");
+	}
+	public void setEdad(int recibe){
+		edad=recibe;
+	}
+	public int getEdad(){
+		return edad;
 	}
 }
