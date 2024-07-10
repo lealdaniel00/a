bd06ab9fd13f4f2c09c20eec347b7c546a1b1db4
lealdaniel00 <lealdaniel00@gmail.com>
2024-07-09 19:45:31 -0500

    Update Pez.java

--- Pez.java
+++ Pez.java
@@ -1,6 +1,6 @@
 public class Pez extends SerVivo{
 	public void respirar(){
-		System.out.println("La respiracion la estoy haciendo por mis bronquios");
+		System.out.println("La respiracion la estoy haciendo por mis bronquios ");
 	}
 	public void reproduccion(){
 		System.out.println("Cuando una mamita quiere mucho a un papito, una semillita .....");
